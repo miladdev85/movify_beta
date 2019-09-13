04b2d6a497643775342a2cc59369cbc14cb3610a
Milad Hashemi <milad.hashemi.dev@gmail.com>
2019-09-13 22:51:42 +0200

    Verify that gender value exists

--- src/PeopleInfo.js
+++ src/PeopleInfo.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 function PeopleInfo({ person }) {
+  console.log(person.gender);
   return (
     <div className="d-none d-md-block col-3 detail__box">
       <p className="font-weight-bold mt-3">Personal Info</p>
       <p className="mb-1 personal__title">Known For</p>
       <p className="small mb-2 personal__text">{person.known_for_department}</p>
       <p className="mb-1 personal__title">Gender</p>
-      <p className="small mb-2 personal__text">{person.gender === 1 ? "Female" : "Male"}</p>
+      <p className="small mb-2 personal__text">
+        {person.gender && person.gender === 1 ? "Female" : "Male"}
+      </p>
       <p className="personal__title mb-1">Birthday</p>
       <p className="small personal__text mb-2">{person.birthday}</p>
       <p className="personal__title mb-1">Place of Birth</p>
